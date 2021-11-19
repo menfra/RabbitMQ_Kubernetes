e71500489ef7fa2e8210b262a0abe797d6b6d924
Frank Mensah <frank.mensah@two-pillars.de>
2021-11-19 15:09:37 +0100

    db names

--- DataAccess/Env/Commons.cs
+++ DataAccess/Env/Commons.cs
@@ -7,14 +7,6 @@ namespace DataAccess.Env
     public static class Commons
     {
         public const string MESSAGE_QUEUE = "Top_Queue";
-        public const string DBName = "PG_Management";
-        public const string RELEASETB = "TB_Release";
-        public const string LOGTB = "TB_ChangeLog";
-        public const string MODULETB = "TB_Module";
-        public const string TENANTSTB = "TB_Tenants";
-        public const string VMTB = "TB_VM";
-        public const string DEPLOYED_MODULETB = "TB_Deployed";
-        public const string ERROR_LOGTB = "TB_ErrorLog";
-        public const string iQUAVIS_InstallTB = "TB_iQUAVISInstall";
+        public const string DBName = "Consumer";
     }
 }
